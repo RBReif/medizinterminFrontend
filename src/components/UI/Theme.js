e0c27de5e7c17179191a06cb43bfe990bd2bcbd4
Christoph Kipfer <christoph.kipfer@tum.de>
2021-07-09 09:27:13 +0200

    Resolved font issues on Safari (Mac)

--- src/components/UI/Theme.js
+++ src/components/UI/Theme.js
@@ -11,7 +11,18 @@ export const Theme = createMuiTheme ({
         }
     },
     typography: {
-        fontFamily: "Inter"
+        fontFamily: [
+            '-apple-system',
+            'BlinkMacSystemFont',
+            '"Segoe UI"',
+            'Roboto',
+            '"Helvetica Neue"',
+            'Arial',
+            'sans-serif',
+            '"Apple Color Emoji"',
+            '"Segoe UI Emoji"',
+            '"Segoe UI Symbol"',
+          ].join(','),
     },
 
     shape: {
