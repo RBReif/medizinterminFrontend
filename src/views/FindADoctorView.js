c661d603838ddb083eccab02212d675f5e0ff9ec
Christoph Kipfer <christoph.kipfer@tum.de>
2021-07-15 15:16:49 +0200

    Redux works now on the loginpatientsview

--- src/views/FindADoctorView.js
+++ src/views/FindADoctorView.js
@@ -17,7 +17,8 @@ import Card from "@material-ui/core/Card";
 import { makeStyles } from "@material-ui/core";
 import image from "../images/professional.jpg"
 import MultiSelectDropdown from "../components/Forms/MultiSelectDropdown";
-import ConfigService from "../services/ConfigService"
+import ConfigService from "../services/ConfigService";
+import {connect, useSelector} from "react-redux";
 
 
 const useStyles = makeStyles((theme) => ({
@@ -62,6 +63,7 @@ const toggles = [
     isActive: false,
   },
 ];
+
 
 const FindADoctorView = () => {
   const [timeslots, setTimeSlots] = useState("");
