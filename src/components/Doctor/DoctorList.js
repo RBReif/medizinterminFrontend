77858e0f4add96fa351664d4394cf98873bd4025
Christoph Kipfer <christoph.kipfer@tum.de>
2021-07-19 23:34:49 +0200

    getting doctors

--- src/components/Doctor/DoctorList.js
+++ src/components/Doctor/DoctorList.js
@@ -1,12 +1,18 @@
-
+import { useEffect, useState } from "react";
+import DoctorService from "../../services/DoctorService";
 
 const DoctorList = (props) => {
-    
-    return(
-        <div>
-        {console.log("DOCTORLIST", props.result.doctor)}
-        </div>
-    );
-}
+  const [doctor, setDoctor] = useState("");
+  const getDoctor = async () => {
+    const doctor = await DoctorService.getDoctor(props.result.doctor);
+    // console.log("RECEIVED DOCTOR", doctor);
+    console.log("RECEIVED DOCTOR", doctor);
+    setDoctor(doctor);
+  };
+
+  console.log(doctor);
+
+  return <div>{console.log("DOCTORLIST", doctor)}</div>;
+};
 
 export default DoctorList;
