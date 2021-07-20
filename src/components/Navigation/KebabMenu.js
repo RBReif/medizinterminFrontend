d71607cf3d1e47ae55fbb58bad06e9f764d50b75
Maximilian Pfleger <maximilian.pfleger@tum.de>
2021-07-20 21:46:50 +0200

    implement different kebab menu items for patient and doctor

--- src/components/Navigation/KebabMenu.js
+++ src/components/Navigation/KebabMenu.js
@@ -1,16 +1,14 @@
 import React, {useEffect, useState} from "react";
-import { makeStyles } from "@material-ui/core/styles";
+import {makeStyles} from "@material-ui/core/styles";
 import PropTypes from "prop-types";
-import { withRouter } from "react-router-dom";
-import { logout } from "../../redux/actions";
-import { Menu, MenuItem, Avatar, Divider } from "@material-ui/core";
-import { connect, useSelector } from "react-redux";
+import {withRouter} from "react-router-dom";
+import {logout} from "../../redux/actions";
+import {Menu, MenuItem, Avatar, Divider} from "@material-ui/core";
+import {connect, useSelector} from "react-redux";
 import ExitToAppIcon from "@material-ui/icons/ExitToApp";
 import VerifiedUserIcon from "@material-ui/icons/VerifiedUser";
-import UserService from "../../services/UserService";
-import PatientService from "../../services/PatientService";
-import DoctorService from "../../services/DoctorService";
-import AppointmentService from "../../services/AppointmentService";
+import {Dashboard, Today} from "@material-ui/icons";
+import TodayIcon from '@material-ui/icons/Today';
 
 const useStyles = makeStyles((theme) => ({
     menuitem: {
@@ -21,6 +19,7 @@ const useStyles = makeStyles((theme) => ({
         marginRight: theme.spacing(1),
     },
 }));
+
 /**
  * Menu for user managment
  * @param {props} props
@@ -40,6 +39,27 @@ function KebabMenu(props) {
         props.history.push("/login-patients");
     };
 
+    const onClickDoctorDashboard = () => {
+        // close this menu
+        props.onClose();
+        // navigate to the doctor dashboard
+        props.history.push("/doctor-dashboard");
+    };
+
+    const onClickPatientDashboard = () => {
+        // close this menu
+        props.onClose();
+        // navigate to the doctor dashboard
+        props.history.push("/dashboard");
+    };
+
+    const onClickCalendar = () => {
+        // close this menu
+        props.onClose();
+        // navigate to the doctor calendar
+        props.history.push("/doctor-dashboard");
+    };
+
     const onClickLogout = () => {
         // trigger redux logout action
         props.dispatch(logout());
@@ -57,32 +77,75 @@ function KebabMenu(props) {
             anchorEl={props.anchorEl}
             onClose={props.onClose}
         >
-            {userData.user
+            {userData?.user?.role == 'DOCTOR'
                 ? [
                     <MenuItem key="user" className={classes.menuitem}>
                         <Avatar src={userData.user.thumbnail}>
                         </Avatar>
                         {userData.user.username}
                     </MenuItem>,
-                    <Divider key="divider" />,
+                    <Divider key="divider"/>,
+                    <MenuItem
+                        key="dashboard"
+                        onClick={onClickDoctorDashboard}
+                        className={classes.menuitem}
+                    >
+                        <Dashboard className={classes.avatar}/>
+                        Dashboard
+                    </MenuItem>,
+                    <Divider key="divider"/>,
+                    <MenuItem
+                        key="calendar"
+                        onClick={onClickCalendar}
+                        className={classes.menuitem}
+                    >
+                        <TodayIcon className={classes.avatar}/>
+                        Calendar
+                    </MenuItem>,
+                    <Divider key="divider"/>,
                     <MenuItem
                         key="logout"
                         onClick={onClickLogout}
                         className={classes.menuitem}
                     >
-                        <ExitToAppIcon className={classes.avatar} />
+                        <ExitToAppIcon className={classes.avatar}/>
                         Logout
                     </MenuItem>
                 ]
-                : [
-                    <MenuItem
-                        key="login"
-                        onClick={onClickLogin}
-                        className={classes.menuitem}
-                    >
-                        <VerifiedUserIcon className={classes.avatar} />
-                        Login
-                    </MenuItem>
+                : userData?.user?.role == 'PATIENT'
+                    ? [<MenuItem key="user" className={classes.menuitem}>
+                        <Avatar src={userData.user.thumbnail}>
+                        </Avatar>
+                        {userData.user.username}
+                    </MenuItem>,
+                        <Divider key="divider"/>,
+                        <MenuItem
+                            key="dashboard"
+                            onClick={onClickPatientDashboard}
+                            className={classes.menuitem}
+                        >
+                            <Dashboard className={classes.avatar}/>
+                            Dashboard
+                        </MenuItem>,
+                        <Divider key="divider"/>,
+                        <MenuItem
+                            key="logout"
+                            onClick={onClickLogout}
+                            className={classes.menuitem}
+                        >
+                            <ExitToAppIcon className={classes.avatar}/>
+                            Logout
+                        </MenuItem>
+                    ]
+                    : [
+                        <MenuItem
+                            key="login"
+                            onClick={onClickLogin}
+                            className={classes.menuitem}
+                        >
+                            <VerifiedUserIcon className={classes.avatar}/>
+                            Login
+                        </MenuItem>
                     ]}
         </Menu>
     );
