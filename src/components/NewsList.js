8530270a49e8e1c3b66f9f3d5ecd0c96dd0da34d
Maximilian Pfleger <maximilian.pfleger@tum.de>
2021-07-25 01:40:51 +0200

    add new item to news list in patient dashboard

--- src/components/NewsList.js
+++ src/components/NewsList.js
@@ -1,9 +1,10 @@
 import ListGroup from 'react-bootstrap/ListGroup'
+import {Container, Col, Row} from "react-bootstrap";
 import React from "react";
 import EmojiObjectsIcon from '@material-ui/icons/EmojiObjects';
 import StarIcon from '@material-ui/icons/Star';
 import AssignmentTurnedInIcon from '@material-ui/icons/AssignmentTurnedIn';
-
+import FlightTakeoffIcon from '@material-ui/icons/FlightTakeoff';
 
 // const UnorderedList = (props) => {
 //     return(['You have one upcoming task this week', 'How was the appointment with Dr. Schulz?', 'Another task in here'].map((breakpoint, idx) => (
@@ -13,16 +14,34 @@ import AssignmentTurnedInIcon from '@material-ui/icons/AssignmentTurnedIn';
 //     )));
 // };
 
+const onClickTravel = () => {
+    window.open("https://www.who.int/travel-advice/vaccines", "_blank");
+};
 const NewsList = (props) => {
-    console.log("NEWSLIST ",props.mostRecent)
+    console.log("NEWSLIST ", props.mostRecent)
     return (
         <ListGroup>
-            <ListGroup.Item> <EmojiObjectsIcon /> You have {props.outstanding} upcoming appointment(s)</ListGroup.Item>
-            { props.mostRecent===null?(
-                <ListGroup.Item><StarIcon /> We have no previous appointments stored from you.</ListGroup.Item>
-            ):(
-            <ListGroup.Item><StarIcon /> How was your most recent appointment (with {props.mostRecent.doctor_name} {props.mostRecent.doctor_last_name})?</ListGroup.Item>)}
-            <ListGroup.Item><AssignmentTurnedInIcon /> Update your Checkup Schedule</ListGroup.Item>
+            <ListGroup.Item> <Row><Col sm={1}><EmojiObjectsIcon/> </Col>
+                <Col>You have {props.outstanding} upcoming appointment(s)</Col>
+            </Row>
+            </ListGroup.Item>
+            {props.mostRecent === null ? (
+                <ListGroup.Item> <Row><Col sm={1}><EmojiObjectsIcon/> </Col>
+                    <Col>We have no previous appointments stored from you.</Col>
+                </Row>
+                </ListGroup.Item>
+            ) : (
+                <ListGroup.Item>
+                    <Row><Col sm={1}><StarIcon/> </Col>
+                        <Col>How was your most recent appointment
+                            (with {props.mostRecent.doctor_name} {props.mostRecent.doctor_last_name})?</Col>
+                    </Row>
+                </ListGroup.Item>)}
+            <ListGroup.Item action onClick={onClickTravel}>
+                <Row><Col sm={1}><FlightTakeoffIcon/> </Col>
+                    <Col>Are you planning to travel? Click here to check out WHO's newest vaccination advices</Col>
+                </Row>
+            </ListGroup.Item>
         </ListGroup>
     )
 };
